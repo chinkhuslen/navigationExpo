eaf368fc6351bb8d66a2a78bbb6d00a6b32a04bd
chinkhuslen <chinkhuslen10@gmail.com>
2023-02-06 10:10:16 +0800

    setup

--- App.js
+++ App.js
@@ -3,13 +3,18 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "./src/screens/Home.js";
+import ScanScreen from "./src/screens/Scan.js";
+import OrderScreen from "./src/screens/Order.js";
+import ProfileScreen from "./src/screens/Profile.js";
 const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Profile" component={HomeScreen} />
+        <Tab.Screen name="Scan" component={ScanScreen} />
+        <Tab.Screen name="Order" component={OrderScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
