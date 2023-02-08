04e3d4ed7897f254f77575420490496690f1bc6d
chinkhuslen <chinkhuslen10@gmail.com>
2023-02-08 12:11:16 +0800

    splash screen

--- App.js
+++ App.js
@@ -1,53 +1,86 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-// import { NavigationContainer } from "@react-navigation/native";
-// import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "./src/screens/Home.js";
 import ScanScreen from "./src/screens/Scan.js";
 import OrderScreen from "./src/screens/Order.js";
 import ProfileScreen from "./src/screens/Profile.js";
 const Tab = createBottomTabNavigator();
-// import { Feather } from "@expo/vector-icons";
 import Ionicons from "@expo/vector-icons/Ionicons";
+
+import React, { useCallback, useEffect, useState } from "react";
+import * as SplashScreen from "expo-splash-screen";
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-    // <NavigationContainer>
-    //   <Tab.Navigator
-    //     screenOptions={({ route }) => ({
-    //       tabBarIcon: ({ focused, color, size }) => {
-    //         let iconName;
-    //         switch (route.name) {
-    //           case "Home":
-    //             iconName = focused ? "ios-home" : "ios-home-outline";
-    //             break;
-    //           case "Profile":
-    //             iconName = focused ? "ios-person" : "ios-person-outline";
-    //             break;
-    //           case "Scan":
-    //             iconName = focused ? "ios-scan" : "ios-scan-outline";
-    //             break;
-    //           case "Order":
-    //             iconName = focused ? "ios-cart" : "ios-cart-outline";
-    //             break;
-    //           default:
-    //             iconName = focused
-    //               ? "ios-help-circle"
-    //               : "ios-help-circle-outline";
-    //             break;
-    //         }
-    //         return <Ionicons name={iconName} size={size} color={color} />;
-    //       },
-    //       tabBarActiveTintColor: "tomato",
-    //       tabBarInactiveTintColor: "gray",
-    //     })}
-    //   >
-    //     <Tab.Screen name="Home" component={HomeScreen} />
-    //     <Tab.Screen name="Scan" component={ScanScreen} />
-    //     <Tab.Screen name="Order" component={OrderScreen} />
-    //     <Tab.Screen name="Profile" component={ProfileScreen} />
-    //   </Tab.Navigator>
-    // </NavigationContainer>
-    <Text>Hi</Text>
+    <SafeAreaView style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+              switch (route.name) {
+                case "Home":
+                  iconName = focused ? "ios-home" : "ios-home-outline";
+                  break;
+                case "Profile":
+                  iconName = focused ? "ios-person" : "ios-person-outline";
+                  break;
+                case "Scan":
+                  iconName = focused ? "ios-scan" : "ios-scan-outline";
+                  break;
+                case "Order":
+                  iconName = focused ? "ios-cart" : "ios-cart-outline";
+                  break;
+                default:
+                  iconName = focused
+                    ? "ios-help-circle"
+                    : "ios-help-circle-outline";
+                  break;
+              }
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: "tomato",
+            tabBarInactiveTintColor: "gray",
+          })}
+        >
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Scan" component={ScanScreen} />
+          <Tab.Screen name="Order" component={OrderScreen} />
+          <Tab.Screen name="Profile" component={ProfileScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
 
